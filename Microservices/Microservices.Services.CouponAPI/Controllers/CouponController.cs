23b5c385553400be593ea66333e6f1924b0f94da
baobibo <ng.hoangbao03@gmail.com>
2024-11-07 10:22:31 +0700

    init

---
+++ Microservices/Microservices.Services.CouponAPI/Controllers/CouponController.cs
@@ -0,0 +1,54 @@
+﻿using Microservices.Services.CouponAPI.Data;
+using Microservices.Services.CouponAPI.Models;
+using Microservices.Services.CouponAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Microservices.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+        private ResponseDto _response;
+
+        public CouponController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+            _response = new ResponseDto();
+        }
+
+        [HttpGet]
+        public ResponseDto Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _appDbContext.Coupons.ToList();
+                _response.Result = objList;
+            }
+            catch (Exception ex)
+            {
+                _response.isSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ResponseDto Get(int id)
+        {
+            try
+            {
+                Coupon objList = _appDbContext.Coupons.First(_ => _.CouponId == id);
+                _response.Result = objList;
+            }
+            catch (Exception ex)
+            {
+                _response.isSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+    }
+}
