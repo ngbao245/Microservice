7ddbd99d23468782c12b06219345b16670a4e40a
baobibo <ng.hoangbao03@gmail.com>
2024-11-18 16:58:10 +0700

    Create Coupon in Action

--- Microservices/Microservices.Web/Controllers/CouponController.cs
+++ Microservices/Microservices.Web/Controllers/CouponController.cs
@@ -1,4 +1,5 @@
-﻿using Microservices.Web.Models;
+﻿using System.Collections.Generic;
+using Microservices.Web.Models;
 using Microservices.Web.Service.IService;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -24,6 +25,27 @@ namespace Microservices.Web.Controllers
 				list = JsonConvert.DeserializeObject <List<CouponDto>>(Convert.ToString(response.Result));
 			}
 			return View(list);
+		}
+
+		public async Task<IActionResult> CouponCreate()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> CouponCreate(CouponDto model)
+		{
+			if (ModelState.IsValid)
+			{
+                ResponseDto? response = await _couponService.CreateCouponAsync(model);
+
+                if (response != null && response.isSuccess)
+                {
+					var a = nameof(CouponIndex);
+                    return RedirectToAction(nameof(CouponIndex));
+                }
+            }
+			return View(model);
 		}
 	}
 }
